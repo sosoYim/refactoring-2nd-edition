dcba31bae610ecc73639b1c5f64d48a0cb8615bc
sosoYim <sohyung.yim@gmail.com>
2023-07-24 20:34:24 +0900

    refactor: ch01-01 함수 추출하기
    - amountFor 함수 추출

--- chapter01/01-1/statement.js
+++ chapter01/01-1/statement.js
@@ -1,3 +1,29 @@
+const amountFor = (aPerformance, play) => {
+  let result = 0;
+  switch (play.type) {
+    case 'tragedy':
+      result = 40_000;
+
+      if (aPerformance.audience > 30) {
+        result += 1_000 * (aPerformance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      result = 30_000;
+
+      if (aPerformance.audience > 20) {
+        result += 10_000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return result
+}
+
+
 export function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -7,28 +33,7 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40_000;
-
-        if (perf.audience > 30) {
-          thisAmount += 1_000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30_000;
-
-        if (perf.audience > 20) {
-          thisAmount += 10_000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf)
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
