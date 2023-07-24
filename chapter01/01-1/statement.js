e330cd4cfeeeb2316d25093aee8774ed913c99c6
sosoYim <sohyung.yim@gmail.com>
2023-07-24 21:07:41 +0900

    refactor: 변수이름 변경

--- chapter01/01-1/statement.js
+++ chapter01/01-1/statement.js
@@ -43,11 +43,11 @@ export function statement(invoice, plays) {
   }
   
   function totalVolumeCredits(){
-    let volumeCredits = 0;
+    let result= 0;
     for(let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf)
+      result+= volumeCreditsFor(perf)
     }
-    return volumeCredits;
+    return result;
   }
   
   function totalAmount(){
