d4f32847ca6f1f67bb0077bfde3760e3bab16236
sosoYim <sohyung.yim@gmail.com>
2023-07-24 20:52:26 +0900

    refactor: 반복문 쪼개기
    - 포인트 누적 부분 반복문 분리

--- chapter01/01-1/statement.js
+++ chapter01/01-1/statement.js
@@ -47,14 +47,14 @@ export function statement(invoice, plays) {
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf)
-
-   volumeCredits += volumeCreditsFor(pert)
-
+    const play = plays[perf.playID]
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${usd(thisAmount)} ${perf.audience}석\n`;
-    totalAmount += thisAmount;
+    result += `${play.name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
+    totalAmount += amountFor(perf);
+  }
+
+  for(let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf)
   }
   result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
