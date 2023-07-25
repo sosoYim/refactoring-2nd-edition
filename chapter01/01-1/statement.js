a26d585a0bf0f460796cc2c2bec1a4974208d037
sosoYim <sohyung.yim@gmail.com>
2023-07-25 20:17:27 +0900

    feat: 데이터 구조체에 invoice 관련 데이터 담기

--- chapter01/01-1/statement.js
+++ chapter01/01-1/statement.js
@@ -1,8 +1,11 @@
 export function statement(invoice, plays) {
-  return renderPlainText(invoice, plays)
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances
+  return renderPlainText(statementData, plays)
 }
 
-function renderPlainText(invoice, plays){
+function renderPlainText(data, plays){
   const playFor = (aPerformance) => {
     return plays[aPerformance.playID];
   }
@@ -48,7 +51,7 @@ function renderPlainText(invoice, plays){
   
   const totalVolumeCredits = () => {
     let result= 0;
-    for(let perf of invoice.performances) {
+    for(let perf of data.performances) {
       result+= volumeCreditsFor(perf)
     }
     return result;
@@ -56,15 +59,15 @@ function renderPlainText(invoice, plays){
   
   const totalAmount = () => {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
   }
 
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+  let result = `청구내역 (고객명: ${data.customer})\n`;
   
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     const play = plays[perf.playID]
     // 청구 내역을 출력한다.
     result += `${play.name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
