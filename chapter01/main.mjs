d51a8697162c4fc713abae0630fc5b7be010e5a7
sosoYim <sohyung.yim@gmail.com>
2023-07-30 15:24:39 +0900

    style: 완성 파일 옮기기

--- chapter01/main.mjs
+++ chapter01/main.mjs
@@ -1,4 +1,4 @@
-import { statement } from './01-1/statement.js';
+import { statement } from './complete/statement.js';
 
 import invoices from './invoices.json' assert { type: 'json' };
 import plays from './plays.json' assert { type: 'json' };
